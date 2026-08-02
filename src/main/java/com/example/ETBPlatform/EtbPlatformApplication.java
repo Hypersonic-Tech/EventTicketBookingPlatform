ba0bf2fe0e234f767f2c6620ab87d81d5d5077a0
Aditya Jain <adityajaindd5@gmail.com>
2026-08-02 21:12:48 +0530

    first commit

---
+++ src/main/java/com/example/ETBPlatform/EtbPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.ETBPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtbPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtbPlatformApplication.class, args);
+	}
+
+}

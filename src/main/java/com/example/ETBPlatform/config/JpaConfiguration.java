399e8f9ce2be8961582d51d19a8e582ce430acb3
Aditya Jain <adityajaindd5@gmail.com>
2026-08-28 18:11:02 +0530

    did jpa auditing and manually added orm.xml

--- src/main/java/com/example/ETBPlatform/config/JpaConfiguration.java
+++ src/main/java/com/example/ETBPlatform/config/JpaConfiguration.java
@@ -5,6 +5,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
+//JPA Auditing saves you from manually writing createdAt, updatedAt, createdBy, and lastModifiedBy
+// logic every time you create or update an entity.
+
+//META-INF/orm.xml is used to define JPA entity mappings/configuration externally using XML instead of annotations.
+//For normal Spring Boot JPA Auditing with @EnableJpaAuditing, you usually don't need it.
 public class JpaConfiguration {
 
 }
